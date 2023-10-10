eb50c99f6c0b0ac662890bbf320fcd5f90ef765d
sota1235 <sota1235@gmail.com>
2023-10-10 22:35:35 +0900

    modify: setting up linting tools

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,152 +2,167 @@ import parse from './index';
 
 test('parsing a proper link header with next and last', () => {
   const link =
-    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100>; rel="next", ' + 
-    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100>; rel="last"'
-  const result = parse(link)
-  expect(result).toEqual(
-    { next:
-        { client_id: '1',
-          client_secret: '2',
-          page: '2',
-          per_page: '100',
-          rel: 'next',
-          url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100' },
-        last:
-        { client_id: '1',
-          client_secret: '2',
-          page: '3',
-          per_page: '100',
-          rel: 'last',
-          url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100' } }
-
-  );
-})
+    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100>; rel="next", ' +
+    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100>; rel="last"';
+  const result = parse(link);
+  expect(result).toEqual({
+    next: {
+      client_id: '1',
+      client_secret: '2',
+      page: '2',
+      per_page: '100',
+      rel: 'next',
+      url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100',
+    },
+    last: {
+      client_id: '1',
+      client_secret: '2',
+      page: '3',
+      per_page: '100',
+      rel: 'last',
+      url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100',
+    },
+  });
+});
 
 test('handles unquoted relationships', () => {
   const link =
-    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100>; rel=next, ' + 
-    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100>; rel=last'
-  const result = parse(link)
-  expect(result).toEqual(
-     { next:
-        { client_id: '1',
-          client_secret: '2',
-          page: '2',
-          per_page: '100',
-          rel: 'next',
-          url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100' },
-        last:
-        { client_id: '1',
-          client_secret: '2',
-          page: '3',
-          per_page: '100',
-          rel: 'last',
-          url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100' } }
-  );
-})
+    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100>; rel=next, ' +
+    '<https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100>; rel=last';
+  const result = parse(link);
+  expect(result).toEqual({
+    next: {
+      client_id: '1',
+      client_secret: '2',
+      page: '2',
+      per_page: '100',
+      rel: 'next',
+      url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=2&per_page=100',
+    },
+    last: {
+      client_id: '1',
+      client_secret: '2',
+      page: '3',
+      per_page: '100',
+      rel: 'last',
+      url: 'https://api.github.com/user/9287/repos?client_id=1&client_secret=2&page=3&per_page=100',
+    },
+  });
+});
 
 test('parsing a proper link header with next, prev and last', () => {
   const linkHeader =
-    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next", ' + 
-    '<https://api.github.com/user/9287/repos?page=1&per_page=100>; rel="prev", ' + 
-    '<https://api.github.com/user/9287/repos?page=5&per_page=100>; rel="last"'
+    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next", ' +
+    '<https://api.github.com/user/9287/repos?page=1&per_page=100>; rel="prev", ' +
+    '<https://api.github.com/user/9287/repos?page=5&per_page=100>; rel="last"';
 
-  const result = parse(linkHeader)
-  
-  expect(result).toEqual(
-    { next:
-        { page: '3',
-          per_page: '100',
-          rel: 'next',
-          url: 'https://api.github.com/user/9287/repos?page=3&per_page=100' },
-        prev:
-        { page: '1',
-          per_page: '100',
-          rel: 'prev',
-          url: 'https://api.github.com/user/9287/repos?page=1&per_page=100' },
-        last:
-        { page: '5',
-          per_page: '100',
-          rel: 'last',
-          url: 'https://api.github.com/user/9287/repos?page=5&per_page=100' } }
-  )
-})
+  const result = parse(linkHeader);
+
+  expect(result).toEqual({
+    next: {
+      page: '3',
+      per_page: '100',
+      rel: 'next',
+      url: 'https://api.github.com/user/9287/repos?page=3&per_page=100',
+    },
+    prev: {
+      page: '1',
+      per_page: '100',
+      rel: 'prev',
+      url: 'https://api.github.com/user/9287/repos?page=1&per_page=100',
+    },
+    last: {
+      page: '5',
+      per_page: '100',
+      rel: 'last',
+      url: 'https://api.github.com/user/9287/repos?page=5&per_page=100',
+    },
+  });
+});
 
 test('parsing an empty link header', () => {
-  const linkHeader = ''
+  const linkHeader = '';
   expect(parse(linkHeader)).toBeNull();
-})
+});
 
 test('parsing a proper link header with next and a link without rel', () => {
   const linkHeader =
-    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next", ' + 
-    '<https://api.github.com/user/9287/repos?page=1&per_page=100>; pet="cat", '
-  const result = parse(linkHeader)
-  
-  expect(result).toEqual(
-    { next:
-        { page: '3',
-          per_page: '100',
-          rel: 'next',
-          url: 'https://api.github.com/user/9287/repos?page=3&per_page=100' } }
-  )
-})
+    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next", ' +
+    '<https://api.github.com/user/9287/repos?page=1&per_page=100>; pet="cat", ';
+  const result = parse(linkHeader);
+
+  expect(result).toEqual({
+    next: {
+      page: '3',
+      per_page: '100',
+      rel: 'next',
+      url: 'https://api.github.com/user/9287/repos?page=3&per_page=100',
+    },
+  });
+});
 
 test('parsing a proper link header with next and properties besides rel', () => {
   const linkHeader =
-    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next"; hello="world"; pet="cat"'
-  const result = parse(linkHeader)
+    '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next"; hello="world"; pet="cat"';
+  const result = parse(linkHeader);
 
-  expect(result).toEqual(
-    { next:
-        { page: '3',
-          per_page: '100',
-          rel: 'next',
-          hello: 'world',
-          pet: 'cat',
-          url: 'https://api.github.com/user/9287/repos?page=3&per_page=100' } }
-  )
-})
+  expect(result).toEqual({
+    next: {
+      page: '3',
+      per_page: '100',
+      rel: 'next',
+      hello: 'world',
+      pet: 'cat',
+      url: 'https://api.github.com/user/9287/repos?page=3&per_page=100',
+    },
+  });
+});
 
 test('parsing a proper link header with a comma in the url', () => {
   const linkHeader =
-    '<https://imaginary.url.notreal/?name=What,+me+worry>; rel="next";'
+    '<https://imaginary.url.notreal/?name=What,+me+worry>; rel="next";';
 
-  const result = parse(linkHeader)
-  
-  expect(result).toEqual(
-    { next:
-        { rel: 'next',
-          name: 'What, me worry',
-          url: 'https://imaginary.url.notreal/?name=What,+me+worry' } }
-  )
-})
+  const result = parse(linkHeader);
+
+  expect(result).toEqual({
+    next: {
+      rel: 'next',
+      name: 'What, me worry',
+      url: 'https://imaginary.url.notreal/?name=What,+me+worry',
+    },
+  });
+});
 
 test('parsing a proper link header with a multi-word rel', () => {
   const linkHeader =
-    '<https://imaginary.url.notreal/?name=What,+me+worry>; rel="next page";'
+    '<https://imaginary.url.notreal/?name=What,+me+worry>; rel="next page";';
 
-  const result = parse(linkHeader)
+  const result = parse(linkHeader);
 
-  expect(result).toEqual(
-    { page: { rel: 'page',
-          name: 'What, me worry',
-          url: 'https://imaginary.url.notreal/?name=What,+me+worry' },
-        next: { rel: 'next',
-          name: 'What, me worry',
-          url: 'https://imaginary.url.notreal/?name=What,+me+worry' }}
-  )
-})
+  expect(result).toEqual({
+    page: {
+      rel: 'page',
+      name: 'What, me worry',
+      url: 'https://imaginary.url.notreal/?name=What,+me+worry',
+    },
+    next: {
+      rel: 'next',
+      name: 'What, me worry',
+      url: 'https://imaginary.url.notreal/?name=What,+me+worry',
+    },
+  });
+});
 
 test('parsing a proper link header with matrix parameters', () => {
   const linkHeader =
-    '<https://imaginary.url.notreal/segment;foo=bar;baz/item?name=What,+me+worry>; rel="next";'
-  const result = parse(linkHeader)
+    '<https://imaginary.url.notreal/segment;foo=bar;baz/item?name=What,+me+worry>; rel="next";';
+  const result = parse(linkHeader);
 
-  expect(result).toEqual(
-     { next: { rel: 'next',
-          name: 'What, me worry',
-          url: 'https://imaginary.url.notreal/segment;foo=bar;baz/item?name=What,+me+worry' }}
-  )
-})
+  expect(result).toEqual({
+    next: {
+      rel: 'next',
+      name: 'What, me worry',
+      url: 'https://imaginary.url.notreal/segment;foo=bar;baz/item?name=What,+me+worry',
+    },
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-const PARSE_LINK_HEADER_MAXLEN = parseInt(process.env.PARSE_LINK_HEADER_MAXLEN ?? '') || 2000;
-const PARSE_LINK_HEADER_THROW_ON_MAXLEN_EXCEEDED = process.env.PARSE_LINK_HEADER_THROW_ON_MAXLEN_EXCEEDED != null
+const PARSE_LINK_HEADER_MAXLEN =
+  parseInt(process.env.PARSE_LINK_HEADER_MAXLEN ?? '') || 2000;
+const PARSE_LINK_HEADER_THROW_ON_MAXLEN_EXCEEDED =
+  process.env.PARSE_LINK_HEADER_THROW_ON_MAXLEN_EXCEEDED != null;
 
 interface RawLink {
   url: string;
@@ -10,9 +12,7 @@ interface Link extends RawLink {
   rel: string;
 }
 
-interface Result {
-  [key: string]: Link;
-}
+type Result = Record<string, Link>;
 
 function hasRel(link: null | Record<string, string>): link is Link {
   if (link === null) {
@@ -22,9 +22,9 @@ function hasRel(link: null | Record<string, string>): link is Link {
   return link.rel != null;
 }
 
-function intoRels (acc: Result, link: Link) {
-  function splitRel (rel: string) {
-    acc[rel] = { ...link, ...{ rel }};
+function intoRels(acc: Result, link: Link) {
+  function splitRel(rel: string) {
+    acc[rel] = { ...link, ...{ rel } };
   }
 
   link.rel.split(/\s+/).forEach(splitRel);
@@ -32,24 +32,24 @@ function intoRels (acc: Result, link: Link) {
   return acc;
 }
 
-function createObjects (acc: Record<string, string>, p: string) {
+function createObjects(acc: Record<string, string>, p: string) {
   // rel="next" => 1: rel 2: next
-  const m = p.match(/\s*(.+)\s*=\s*"?([^"]+)"?/)
+  const m = p.match(/\s*(.+)\s*=\s*"?([^"]+)"?/);
   if (m) acc[m[1]] = m[2];
   return acc;
 }
 
-function parseLink(link: string) : RawLink | null {
+function parseLink(link: string): RawLink | null {
   try {
-    const m         =  link.match(/<?([^>]*)>(.*)/)
+    const m = link.match(/<?([^>]*)>(.*)/);
 
     if (m === null) {
-      console.warn('Invalid link header')
+      console.warn('Invalid link header');
       return null;
     }
 
-    const linkUrl   =  m[1];
-    const parts     =  m[2].split(';');
+    const linkUrl = m[1];
+    const parts = m[2].split(';');
     const parsedUrl = new URL(linkUrl);
     const qry: Record<string, string> = {};
 
@@ -59,9 +59,8 @@ function parseLink(link: string) : RawLink | null {
 
     parts.shift();
 
-    let info = parts
-      .reduce<Record<string, string>>(createObjects, {});
-    
+    let info = parts.reduce<Record<string, string>>(createObjects, {});
+
     info = { ...qry, ...info, url: linkUrl };
     return info as RawLink;
   } catch (e) {
@@ -69,13 +68,17 @@ function parseLink(link: string) : RawLink | null {
   }
 }
 
-function checkHeader(linkHeader: string){
+function checkHeader(linkHeader: string) {
   if (linkHeader.length > PARSE_LINK_HEADER_MAXLEN) {
     if (PARSE_LINK_HEADER_THROW_ON_MAXLEN_EXCEEDED) {
-      throw new Error('Input string too long, it should be under ' + PARSE_LINK_HEADER_MAXLEN + ' characters.');
+      throw new Error(
+        'Input string too long, it should be under ' +
+          PARSE_LINK_HEADER_MAXLEN +
+          ' characters.',
+      );
     } else {
-        return false;
-      }
+      return false;
+    }
   }
   return true;
 }
@@ -87,8 +90,9 @@ export default function (linkHeader: string): Result | null {
 
   if (!checkHeader(linkHeader)) return null;
 
-  return linkHeader.split(/,\s*</)
-   .map(parseLink)
-   .filter<Link>(hasRel)
-   .reduce(intoRels, {});
-};
+  return linkHeader
+    .split(/,\s*</)
+    .map(parseLink)
+    .filter<Link>(hasRel)
+    .reduce(intoRels, {});
+}
